2d6b81b47066e23269505384f8eb5a550fa2316a
md shakil hossain <shakil18046@yahoo.com>
2022-05-14 16:49:03 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Navbar from "./component/Navbar";
+import Home from "./pages/Home";
+import { Route, Routes } from "react-router-dom";
+import About from "./pages/About";
+import Services from "./pages/Services";
+import Contact from "./pages/Contact";
+import Login from "./pages/Login";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar>
+        <Routes>
+          <Route path="/" element={<Home></Home>}></Route>
+          <Route path="/about" element={<About></About>}></Route>
+          <Route path="/Services" element={<Services></Services>}></Route>
+          <Route path="/Conatct" element={<Contact></Contact>}></Route>
+          <Route path="/Login" element={<Login></Login>}></Route>
+        </Routes>
+      </Navbar>
     </div>
   );
 }
---
+++ src/component/Navbar.js
@@ -0,0 +1,67 @@
+import React from "react";
+
+const Navbar = ({ children }) => {
+  return (
+    <div>
+      <div className="drawer drawer-end">
+        <input id="my-drawer-3" type="checkbox" className="drawer-toggle" />
+        <div className="drawer-content flex flex-col">
+          <div className="w-full navbar bg-base-300">
+            <div className="flex-1 px-2 mx-2">clean co</div>
+            <div className="flex-none lg:hidden">
+              <label for="my-drawer-3" className="btn btn-square btn-ghost">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  className="inline-block w-6 h-6 stroke-current"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M4 6h16M4 12h16M4 18h16"
+                  ></path>
+                </svg>
+              </label>
+            </div>
+
+            <div className="flex-none hidden lg:block">
+              <ul className="menu menu-horizontal">
+                <li>
+                  <a className="rounded-lg">Home</a>
+                </li>
+                <li>
+                  <a className="rounded-lg">About</a>
+                </li>
+                <li>
+                  <a className="rounded-lg">Services</a>
+                </li>
+                <li>
+                  <a className="rounded-lg">Contact</a>
+                </li>
+                <li>
+                  <a className="rounded-lg">Login</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+          {children}
+        </div>
+        <div className="drawer-side">
+          <label for="my-drawer-3" className="drawer-overlay"></label>
+          <ul className="menu p-4 overflow-y-auto w-80 bg-base-100">
+            <li>
+              <a>Sidebar Item 1</a>
+            </li>
+            <li>
+              <a>Sidebar Item 2</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
